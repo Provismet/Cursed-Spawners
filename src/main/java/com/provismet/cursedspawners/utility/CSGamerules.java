2b8580238ddb73622c8e6e565a4e07ffabfb8039
Provismet <17149901+Provismet@users.noreply.github.com>
2024-08-12 21:45:43 +0100

    Adjust default gamerule values.

--- src/main/java/com/provismet/cursedspawners/utility/CSGamerules.java
+++ src/main/java/com/provismet/cursedspawners/utility/CSGamerules.java
@@ -14,13 +14,13 @@ import net.minecraft.world.GameRules;
 
 public abstract class CSGamerules {
     public static final CustomGameRuleCategory SPAWNER_CATEGORY = new CustomGameRuleCategory(CursedSpawnersMain.identifier("gamerule_category"), Text.translatable("gamerule.category.cursed-spawners").formatted(Formatting.BOLD, Formatting.YELLOW));
-    public static final GameRules.Key<DoubleRule> MIMIC_CHANCE = GameRuleRegistry.register("spawnerMimicChance", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(0.05, 0, 1));
-    public static final GameRules.Key<DoubleRule> BREAK_SPEED = GameRuleRegistry.register("spawnerMiningSpeedModifier", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(1, 0, (server, rule) -> {
+    public static final GameRules.Key<DoubleRule> MIMIC_CHANCE = GameRuleRegistry.register("spawnerMimicChance", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(0.1, 0, 1));
+    public static final GameRules.Key<DoubleRule> BREAK_SPEED = GameRuleRegistry.register("spawnerMiningSpeedModifier", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(0.5, 0, (server, rule) -> {
         for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
             ServerPlayNetworking.send(player, new GameRulePayloadS2C((float)rule.get()));
         }
     }));
-    public static final GameRules.Key<DoubleRule> SPAWNER_ACTION_CHANCE = GameRuleRegistry.register("spawnerActionChance", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(0.25, 0, 1));
+    public static final GameRules.Key<DoubleRule> SPAWNER_ACTION_CHANCE = GameRuleRegistry.register("spawnerActionChance", SPAWNER_CATEGORY, GameRuleFactory.createDoubleRule(0.333, 0, 1));
 
     public static void init () {}
 }
